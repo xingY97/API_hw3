c21f8f4db0e67ee71d2f1b6cc04053d239356e64
Xing_Ye <YOUR_PERSONAL_EMAIL@example.com>
2019-11-19 14:30:06 -0800

    fix bugs

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
 from flask import Flask, render_template, request
+import pprint
 
 
 app = Flask(__name__)
 
+pp = pprint.PrettyPrinter(indent=4)
+weather_url = "https://api.openweathermap.org/data/2.5/weather"
 
-def kelvin_to_fahrenheit(temp_kelvin):
-    temp_kelvin = main_data["temp"]
-    temp_fahrenheit = temp_in_kelvin * 9/5 - 459.67
-    return temp_fahrenheit
 
 @app.route('/')
 def index():
@@ -18,7 +17,7 @@ def display_weather():
     return render_template("weather_form.html")
 
 
-@app.route('/weather_results', methods=["GET"])
+@app.route('/weather_results', methods=['GET', 'POST'])
 def weather_results_page():
     users_city = request.args.get('city')
 
@@ -26,24 +25,18 @@ def weather_results_page():
         'q':users_city,
         'appid' : d418aaba4e9cf886d4e98d9b56a738b8
     }
-    response = requests.get('http://api.openweathermap.org/data/2.5/weather', params=params)
-    response_json = response.json()
-    
-    main_data = response_json["main"]
-    temp_in_kelvin = main_data["temp"]
-    temp_in_farenheit = temp_in_kelvin *9/5 - 459.67
-    print("the temperature is" + str(temp_in_kelvin) + "in kelvin.")
-    return render_template("/weather_results.html", city=city, temperature=int(temp_fahrenheit))
-    
-
-    # if not r.status_code == 200:
-    #     print("error")
-    # results = r.json()
-    # city = results['name']
-    # temp = kelvin_to_farenheit(results['main']['temp'])
-    # return render_template('weather_results.html', city=city, temp=temp)
+    r = requests.get('http://api.openweathermap.org/data/2.5/weather', params=params)
+    if not r.status_code == 200:
+        print("error")
+    results = r.json()
+    city = results['name']
+    temp = kelvin_to_farenheit(results['main']['temp'])
+    return render_template('weather_results.html', city=city, temp=temp)
 
 
+def kelvin_to_farenheit(k):
+    results = 1.8 * (k-273) + 32
+    return int(results)
 
 
 
